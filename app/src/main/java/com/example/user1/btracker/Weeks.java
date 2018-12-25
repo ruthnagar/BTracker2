f4830f0d01293283e6ff72f52e6e3ecb23f3ce24
Ruth <ruth050311@gmail.com>
2018-12-25 12:59:05 +0200

    add weeks activity

---
+++ app/src/main/java/com/example/user1/btracker/Weeks.java
@@ -0,0 +1,13 @@
+package com.example.user1.btracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Weeks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weeks);
+    }
+}
